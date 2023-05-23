c75c2e395e045530615c934428782a3756c5bfff
Kristoffer Aas <kristoffer.aas@fortedigital.com>
2023-05-23 11:45:08 +0200

    Adds sanity studio

--- apps/www/lib/sanity.ts
+++ apps/www/lib/sanity.ts
@@ -1,16 +1,12 @@
 import { createClient, SanityClient } from "next-sanity";
 import { cache } from "react";
-
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
-const IS_PRODUCTION = process.env.NODE_ENV === "production";
+import { env } from "./env";
 
 const client: SanityClient = createClient({
-  projectId,
-  dataset,
-  apiVersion, // https://www.sanity.io/docs/api-versioning
-  useCdn: IS_PRODUCTION, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
+  projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: env.NEXT_PUBLIC_SANITY_API_VERSION, // https://www.sanity.io/docs/api-versioning
+  useCdn: env.NODE_ENV === "production", // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
 });
 
 // Wrap the cache function in a way that reuses the TypeScript definitions
---
+++ apps/www/sanity.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, Config } from "sanity";
+import { deskTool } from "sanity/desk";
+import { schemaTypes } from "./schemas";
+import { env } from "./lib/env";
+
+export default defineConfig({
+  basePath: "/studio", // <-- important that `basePath` matches the route you're mounting your studio from, it applies to both `/pages` and `/app`
+
+  name: "default",
+  title: "darkred-jackal",
+
+  projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: env.NEXT_PUBLIC_SANITY_DATASET,
+
+  plugins: [deskTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+}) as Config;
