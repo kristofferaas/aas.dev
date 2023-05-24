8da2370cb277616fe0ddcabf750da6ca6a9002cd
Kristoffer Aas <kristoffer.aas@fortedigital.com>
2023-05-24 16:27:48 +0200

    Adds article component

--- apps/www/app/articles/[slug]/page.tsx
+++ apps/www/app/articles/[slug]/page.tsx
@@ -1,33 +1,14 @@
 import { cachedFetch } from "@/lib/sanity";
-import { z } from "zod";
-
-const blockSchema = z
-  .object({
-    _key: z.string(),
-    _type: z.enum(["block"]),
-    style: z.enum(["normal"]),
-    markDefs: z.array(z.unknown()),
-    children: z.array(z.object({ _type: z.enum(["span"]), text: z.string() })),
-  })
-  .passthrough();
-
-type Block = z.infer<typeof blockSchema>;
-
-const articleSchema = z
-  .object({
-    _id: z.string(),
-    title: z.string(),
-    body: blockSchema.array(),
-  })
-  .passthrough();
-
-type Article = z.infer<typeof articleSchema>;
+import Article from "./article";
+import type { ArbitraryTypedObject } from "@portabletext/types";
 
 const getArticleBySlug = async (slug: string) => {
-  const data = await cachedFetch<Article>("*[slug.current == $slug][0]", {
-    slug,
-  });
-  return articleSchema.parse(data);
+  return await cachedFetch<ArbitraryTypedObject>(
+    "*[slug.current == $slug][0]",
+    {
+      slug,
+    }
+  );
 };
 
 export default async function ArticlePage({
@@ -38,13 +19,9 @@ export default async function ArticlePage({
   const article = await getArticleBySlug(params.slug);
 
   return (
-    <article className="container max-w-2xl my-8">
+    <div className="container max-w-2xl my-8">
       <h1 className="text-2xl font-semibold my-8">{article.title}</h1>
-      <ul className="space-y-4">
-        {article.body.map((block) => (
-          <li key={block._key}>{block.children.map((child) => child.text)}</li>
-        ))}
-      </ul>
-    </article>
+      <Article value={article.body} />
+    </div>
   );
 }
