5b92a2a563cff8041463ce45bf55df8b2134b90d
Kristoffer Aas <kristoffer.aas@fortedigital.com>
2023-06-20 13:29:20 +0200

    Adds initial shared tailwind ui package

---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,7 @@
+export const Button = () => {
+  return (
+    <button className="ui-bg-red-400" onClick={() => alert("boop")}>
+      Boop
+    </button>
+  );
+};
---
+++ packages/ui/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  splitting: true,
+  entry: ["src/**/*.tsx"],
+  format: ["esm"],
+  dts: true,
+  minify: true,
+  clean: true,
+  external: ["react"],
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client";',
+    };
+  },
+});
